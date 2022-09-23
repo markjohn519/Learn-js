a6b6fcfb582d685602adafb23d68e3520356df34
Mark John Paul <markjohnmonton@gmail.com>
2022-09-23 14:33:53 +0800

    using class with static and Math.hypot

--- class/class.js
+++ class/class.js
@@ -1,21 +1,28 @@
-class Rectangle {
-  constructor(width, height){
-    this.width = width
-    this.height = height
+class Point {
+  constructor(x, y){
+    this.x = x
+    this.y = y
   }
-  get area() {
-    return this.calcArea()
-  }
-  calcArea() {
-    return this.height * this.width
+  // get area() {
+  //   return this.calcArea()
+  // }
+  // calcArea() {
+  //   return this.height * this.width
+  // }
+  static distance(a,b){
+    const dx = a.x - b.x
+    const dy = a.y - b.y
+    return Math.hypot(dx,dy)
   }
 }
 
 // const square = new Rectangle(2)
 // const poster = new Rectangle()
-const square = new Rectangle(5,5)
+// const square = new Rectangle(5,5)
 
+const p1 = new Point(7,2)
+const p2 = new Point(3,8)
 
 // console.log(square)
 // console.log(poster)
-console.log(square.area)
+console.log(Point.distance(p1,p2))
